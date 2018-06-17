69f541e6721e9b9c1f7c9aae9e9b5fdb5f39efb5
vegfjogs <vegard.fjogstad@finn.no>
2018-06-17 21:22:14 +0200

    Remove babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,15 +35,6 @@ module.exports = {
                 ]
             },
             {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: "babel-loader"
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
                 test: /\.css$/,
                 use: [
                     {
