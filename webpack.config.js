9feb694e9de600903a0ba127c02a547b5b045d17
vegfjogs <vegard.fjogstad@finn.no>
2018-06-18 23:22:22 +0200

    Remove HTMLWebpackPlugin and use full html loader instead

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: [
-            'webpack-hot-middleware/client?reload=true',
-            './src/client/main.ts'
-        ]
+        main: ['webpack-hot-middleware/client?reload=true', './src/client/main.ts'],
     },
     mode: 'development',
     output: {
@@ -20,59 +16,63 @@ module.exports = {
         overlay: true,
         hot: true,
         stats: {
-            colors: true
-        }
+            colors: true,
+        },
     },
-    devtool: "source-map",
+    devtool: 'source-map',
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 use: [
                     {
-                        loader: "ts-loader"
-                    }
-                ]
+                        loader: 'ts-loader',
+                    },
+                ],
             },
             {
                 test: /\.css$/,
                 use: [
                     {
-                        loader: "style-loader"
+                        loader: 'style-loader',
                     },
                     {
-                        loader: "css-loader"
-                    }
-                ]
+                        loader: 'css-loader',
+                    },
+                ],
             },
             {
                 test: /\.html$/,
                 use: [
                     {
-                        loader: "html-loader",
+                        loader: 'file-loader',
                         options: {
-                            attrs: ["img:src"]
-                        }
-                    }
-                ]
+                            name: '[name].[ext]',
+                        },
+                    },
+                    {
+                        loader: 'extract-loader',
+                        options: {
+                            publicPath: '/',
+                        },
+                    },
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
             },
             {
                 test: /\.(jpg|gif|png)$/,
                 use: [
                     {
-                        loader: "file-loader",
+                        loader: 'file-loader',
                         options: {
-                            name: "images/[name].[ext]"
-                        }
-                    }
-                ]
-            }
-        ]
+                            name: 'images/[name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HTMLWebpackPlugin({
-            template: "./src/client/index.html"
-        })
-    ]
-}
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+};
